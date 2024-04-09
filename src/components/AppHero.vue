c22b19c4fe3c9cf037050bba67816e619b789bb0
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-04-09 18:01:26 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    data(){
+        return{
+            message: "La mia prima volta con VITE"
+        }
+    }
+}
+</script>
+<template>
+<div id="AppHero">
+    <img src="https://picsum.photos/900/1000" alt="">
+</div>
+</template>
+<style>
+
+</style>
